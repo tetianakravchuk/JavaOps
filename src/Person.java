b12ad8a20753a2c0ceb8a88cc20c33c17d470ecd
Tetiana <tetiana.kravchukqa@gmail.com>
2020-01-21 19:21:25 -0500

    Home Work 2.2

---
+++ src/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    String sex = "male";
+    String name = "John";
+    int height = 180;
+    int weight = 75;
+    int age = 32;
+
+    public void seat() {
+
+    }
+
+    public void walk() {
+
+    }
+
+    public void run() {
+
+    }
+
+    public void talk() {
+
+    }
+
+    public void learnJava() {
+
+    }
+
+}
