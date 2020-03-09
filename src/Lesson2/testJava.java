01325d4011865b2c8f805de6161294cc124d0d6c
Jaifar <tetiana.kravchukqa@gmail.com>
2020-03-09 17:02:30 -0400

    test one sublime merge app

---
+++ src/Lesson2/testJava.java
@@ -0,0 +1,13 @@
+testJava.java
+
+public class testJava {
+    public static void main(String[] args) {
+        Robot robot = new Robot();
+
+        robot.setModelName("fgg");
+        robot.setWeight(3);
+        robot.setSpeed(34);
+
+    }
+
+}
