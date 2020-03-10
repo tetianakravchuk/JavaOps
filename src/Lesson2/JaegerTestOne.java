c4f9f7c01ee8d134408b4a9200616acbedd319f2
Tetiana <tetiana.kravchukqa@gmail.com>
2020-03-09 20:44:20 -0400

    Corrected

---
+++ src/Lesson2/JaegerTestOne.java
@@ -0,0 +1,28 @@
+package Lesson2;
+
+public class JaegerTestOne {
+    String modelName = "Bracer Phonix";
+    String mark = "Mark-5";
+    String origin = "USA";
+    double height = 70.7;
+    double weight = 2.1;
+    int speed = 3;
+    int strength = 8;
+    int armor = 9;
+
+    void drift(){
+
+    }
+
+    void move(){
+
+    }
+
+    void scanKaiju(){
+
+    }
+
+    void useVortexCannon(){
+
+    }
+}
