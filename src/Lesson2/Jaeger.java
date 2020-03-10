434ec50fd1ac1899f8e112e385da07f74fa65b08
Tetiana <tetiana.kravchukqa@gmail.com>
2020-03-09 20:42:51 -0400

    Corrected

--- src/Lesson2/Jaeger.java
+++ src/Lesson2/Jaeger.java
@@ -1,25 +1,70 @@
 package Lesson2;
 
 public class Jaeger {
-    String modelName = "Bracer Phonix";
-    String mark = "Mark-5";
-    String origin = "USA";
-    double height = 70.7;
-    double weight = 2.1;
-    int speed = 3;
-    int strength = 8;
-    int armor = 9;
+    private String modelName;
+    private String mark;
+    private String origin;
+    private int height;
+    private int weight;
+    private int speed;
+    private int strength;
+    private int armor;
 
-    void drift(){
-
+    public String getModelName() {
+        return modelName;
     }
-    void move(){
 
+     public void setModelName() {
+        this.modelName = modelName;
     }
-    void scanKaiju(){
 
+    public String getOrigin() {
+        return origin;
     }
-    void useVortexCannon(){
 
+    public void setOrigin() {
+        this.origin = origin;
+    }    
+
+    public int getHeight() {
+        return height;
     }
+
+
+    public void setHeight() {
+        this.height = height;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed() {
+        this.speed = speed;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength() {
+        this.strength = strength;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight() {
+        this.weight = weight;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor() {
+        this.armor = armor;
+    }
+    
 }
